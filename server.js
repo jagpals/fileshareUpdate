0986e847fddf654af34bdecd391bcfe6a8799055
Daniyal Mahmood <daniyalmahmood123@gmail.com>
2021-08-31 18:57:04 +0530

    ready to push

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 //routes
-app.use('/api/files',require('./routes/files'));
-app.use('/files',require('./routes/show'));
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
 
 app.use('/files/download', require('./routes/download'));
 app.listen(port,()=>{
